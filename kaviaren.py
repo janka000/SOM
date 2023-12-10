85ddaa3beb2f2329bbfe60e252301943b117eeb5
janka <14cernikova@gmail.com>
2023-12-10 18:31:01 +0100

    kaviaren - nieco funkcne..

--- kaviaren.py
+++ kaviaren.py
@@ -1,90 +1,79 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.patches import Circle
 
-def differential_evolution(objective_function, bounds, constraints, population_size=10, crossover_prob=0.8, max_generations=100, draw_after_each_iteration=False):
-    # Inicializácia populácie
-    population = np.random.uniform(low=bounds[:, 0], high=bounds[:, 1], size=(population_size, len(bounds)))
+def differential_evolution_table_placement(Lx, Ly, r, n, max_generations=100, draw_after_each_iteration=False):
+    # Initialization of the population
+    population = np.random.uniform(low=r, high=(Lx - r, Ly - r), size=(n, 2))
+
+    optimal_positions = []
 
     for generation in range(max_generations):
-        # Výpočet hodnôt funkcie pre každého jedinca
-        fitness_values = np.array([objective_function(individual) for individual in population])
+        for i in range(n):
+            # Mutation
+            indices = [idx for idx in range(n) if idx != i]
+            a, b, c = np.random.choice(indices, 3, replace=False)
+            mutant = population[a] + 0.5 * (population[b] - population[c])
 
-        # Aplikácia obmedzení
-        for i, individual in enumerate(population):
-            if not constraints(individual):
-                fitness_values[i] = float('-inf')
+            # Crossover
+            crossover_prob = np.random.rand()
+            trial = np.where(np.random.rand(2) < crossover_prob, mutant, population[i])
 
-        # Výber rodičov
-        parents = population[np.argsort(fitness_values)[::-1][:2]]
-
-        # Vytvorenie potomkov pomocou kríženia
-        children = crossover(parents[0], parents[1], crossover_prob)
-
-        # Aplikácia mutácie na potomkov
-        children = mutate(children, bounds)
-
-        # Nahradenie najhorších potomkov v populácii
-        worst_indices = np.argsort(fitness_values)[:2]
-        population[worst_indices] = children
+            # Selection
+            if objective_function(trial, Lx, Ly, r) > objective_function(population[i], Lx, Ly, r) and constraints(trial, Lx, Ly, r):
+                population[i] = trial
 
         if draw_after_each_iteration:
-            # Vykreslenie výsledkov po každej iterácii
-            plot_results(bounds, population, Lx, Ly, r)
+            # Visualization of results after each iteration
+            plot_results(Lx, Ly, population, r)
 
-    # Nájdenie najlepšieho riešenia
-    best_index = np.argmax(fitness_values)
-    best_solution = population[best_index]
+        # Finding the best solution
+        best_index = np.argmax([objective_function(ind, Lx, Ly, r) for ind in population])
+        optimal_positions.append(tuple(population[best_index]))
 
     if not draw_after_each_iteration:
-        # Vykreslenie výsledkov na konci
-        plot_results(bounds, population, Lx, Ly, r)
+        # Visualization of final results
+        plot_results(Lx, Ly, population, r)
 
-    return best_solution
+    return optimal_positions
 
-def crossover(parent1, parent2, crossover_prob):
-    # Uniformné kríženie
-    mask = np.random.rand(len(parent1)) < crossover_prob
-    child1 = np.where(mask, parent1, parent2)
-    child2 = np.where(mask, parent2, parent1)
-    return np.array([child1, child2])
+def objective_function(positions, Lx, Ly, r):
+    # Objective function - negative of the minimum distance between table centers
+    min_distance = float('inf')
 
-def mutate(children, bounds, mutation_prob=0.1):
-    # Mutácia v jednom bode
-    mask = np.random.rand(*children.shape) < mutation_prob
-    children += mask * np.random.uniform(low=-1, high=1, size=children.shape) * (bounds[:, 1] - bounds[:, 0])
-    children = np.clip(children, bounds[:, 0], bounds[:, 1])
-    return children
+    for i in range(len(positions)):
+        for j in range(i + 1, len(positions)):
+            distance = np.linalg.norm(positions[i] - positions[j]) - 2 * r
+            min_distance = min(min_distance, distance)
 
-def objective_function(positions):
-    # Funkcia, ktorú chceme maximalizovať (vzdialenosť dvoch najbližších stolov)
-    total_distance = 0
-    n = len(positions) // 2
+    return -min_distance
 
-    for i in range(n):
-        for j in range(i + 1, n):
-            distance = np.sqrt((positions[i] - positions[j]) ** 2 + (positions[i + n] - positions[j + n]) ** 2)
-            total_distance += max(0, distance - 2 * r)  # Berieme do úvahy polomer stola
-
-    return -total_distance  # Minimizujeme negatívnu vzdialenosť, aby sme maximalizovali vzdialenosť
-
-def constraints(positions):
-    # Obmedzenia pre vzdialenosť stolu od hraníc obdĺžnika
-    for i in range(len(positions) // 2):
-        if positions[i] - r < 0 or positions[i] + r > Lx or positions[i + len(positions) // 2] - r < 0 or positions[i + len(positions) // 2] + r > Ly:
+def constraints(positions, Lx, Ly, r):
+    # Constraints for the distance of the table from the boundaries of the cafe
+    if isinstance(positions, list):
+        # For a list of positions
+        for pos in positions:
+            if pos[0] - r < 0 or pos[0] + r > Lx or pos[1] - r < 0 or pos[1] + r > Ly:
+                return False
+    else:
+        # For a single position (tuple)
+        if positions[0] - r < 0 or positions[0] + r > Lx or positions[1] - r < 0 or positions[1] + r > Ly:
             return False
+
     return True
 
-def plot_results(bounds, population, Lx, Ly, r):
+def plot_results(Lx, Ly, positions, r):
     fig, ax = plt.subplots()
     ax.set_xlim(0, Lx)
     ax.set_ylim(0, Ly)
 
-    for pos in population:
-        circle = plt.Circle((pos[0], pos[1]), r, fill=False, color='blue')
+    # Plotting tables
+    for pos in positions:
+        circle = Circle((pos[0], pos[1]), r, fill=False, color='blue')
         ax.add_artist(circle)
 
-    # Obdĺžnik predstavujúci kaviareň
-    rectangle = plt.Rectangle((0, 0), bounds[0][1], bounds[1][1], fill=False, edgecolor='red', linewidth=2)
+    # Plotting the cafe boundaries
+    rectangle = plt.Rectangle((0, 0), Lx, Ly, fill=False, edgecolor='red', linewidth=2)
     ax.add_artist(rectangle)
 
     plt.gca().set_aspect('equal', adjustable='box')
@@ -94,13 +83,11 @@ def plot_results(bounds, population, Lx, Ly, r):
     plt.show()
 
 if __name__ == "__main__":
-    Lx = 20  # Šírka kaviarne
-    Ly = 20  # Výška kaviarne
-    r = 1    # Polomer stola
-    n = 5    # Počet stolov
+    Lx = 30  # Width of the cafe
+    Ly = 20  # Height of the cafe
+    r = 1    # Radius of the table
+    n = 5    # Number of tables
 
-    bounds = np.array([[0, Lx], [0, Ly]] * n)
-    optimal_solution = differential_evolution(objective_function, bounds, constraints, draw_after_each_iteration=True)
-
-    print("Optimal positions:", optimal_solution)
+    optimal_positions = differential_evolution_table_placement(Lx, Ly, r, n, draw_after_each_iteration=True)
+    print("Optimal positions:", optimal_positions)
 
